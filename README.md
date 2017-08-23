3c696eec8d7d389cd1ed1be59015bf45283a678c
Jon Stevens <latchkey@gmail.com>
2017-08-23 19:07:13 +0700

    formatting, dependencies, documentation, lock file

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Electron Skeleton
 
+Based on `electron-forge init electron-skeleton --template vue` with a couple small modifications to support TypeScript by default.
+
 1. `brew install yarn`
 1. `yarn install`
 1. `tsc`
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as Electron from 'electron';
-import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer';
-import { enableLiveReload } from 'electron-compile';
+import installExtension, {VUEJS_DEVTOOLS} from 'electron-devtools-installer';
+import {enableLiveReload} from 'electron-compile';
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -11,28 +11,28 @@ const isDevMode = process.execPath.match(/[\\/]electron/);
 if (isDevMode) enableLiveReload();
 
 const createWindow = async () => {
-  // Create the browser window.
-  mainWindow = new Electron.BrowserWindow({
-    width: 800,
-    height: 600,
-  });
+	// Create the browser window.
+	mainWindow = new Electron.BrowserWindow({
+		width: 800,
+		height: 600,
+	});
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/index.html`);
+	// and load the index.html of the app.
+	mainWindow.loadURL(`file://${__dirname}/index.html`);
 
-  // Open the DevTools.
-  if (isDevMode) {
-    await installExtension(VUEJS_DEVTOOLS);
-    mainWindow.webContents.openDevTools();
-  }
+	// Open the DevTools.
+	if (isDevMode) {
+		await installExtension(VUEJS_DEVTOOLS);
+		mainWindow.webContents.openDevTools();
+	}
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
+	// Emitted when the window is closed.
+	mainWindow.on('closed', () => {
+		// Dereference the window object, usually you would store windows
+		// in an array if your app supports multi windows, this is the time
+		// when you should delete the corresponding element.
+		mainWindow = null;
+	});
 };
 
 // This method will be called when Electron has finished
@@ -42,19 +42,19 @@ Electron.app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 Electron.app.on('window-all-closed', () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-      Electron.app.quit();
-  }
+	// On OS X it is common for applications and their menu bar
+	// to stay active until the user quits explicitly with Cmd + Q
+	if (process.platform !== 'darwin') {
+		Electron.app.quit();
+	}
 });
 
 Electron.app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    return createWindow();
-  }
+	// On OS X it's common to re-create a window in the app when the
+	// dock icon is clicked and there are no other windows open.
+	if (mainWindow === null) {
+		return createWindow();
+	}
 });
 
 // In this file you can include the rest of your app's specific main process
